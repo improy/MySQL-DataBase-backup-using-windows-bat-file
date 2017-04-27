34205500fa0e8107ef5b7da594812403ebae3386
PRoy <improy@users.noreply.github.com>
2017-04-27 20:27:33 +0600

    Update MySqlBackup.bat

--- MySqlBackup.bat
+++ MySqlBackup.bat
@@ -1,36 +1,52 @@
 @echo off
 
- set dbUser=root
- set dbPassword=password-if-any
- set backupDir="D:\LOCALHOST_MySQL_DB_Backup\"
- set mysqldump="D:\wamp\bin\mysql\mysql5.6.17\bin\mysqldump.exe"
- set mysqlDataDir="D:\wamp\bin\mysql\mysql5.6.17\data"
- set zip="C:\Program Files\7-Zip\7z.exe"
+:: make sure to change the settings from line 4-9
+set dbUser=root
+set dbPassword=""
+set backupDir="D:\Localhost_MySQL_Backup"
+set mysqldump="D:\wamp\bin\mysql\mysql5.7.14\bin\mysqldump.exe"
+set mysqlDataDir="D:\wamp\bin\mysql\mysql5.7.14\data"
+set zip="C:\Program Files\7-Zip\7z.exe"
 
- set datetimef=%date%
- set datetimef=%datetimef: =0%
- :: if the directory structure is not correct comment the above line and uncomment the following two lines
- :: set datetimef=%datetimef: =_%
- :: set datetimef=%datetimef:/=_%
+:: get date
+for /F "tokens=2-4 delims=/ " %%i in ('date /t') do (
+	set mm=%%i
+	set dd=%%j
+	set yy=%%k
+)
 
- echo dirName=%datetimef%
- set dirName=%datetimef%
- set fileSuffix=%datetimef%
- 
- :: switch to the "data" folder
- pushd %mysqlDataDir%
+if %mm%==01 set Month="Jan"
+if %mm%==02 set Month="Feb"
+if %mm%==03 set Month="Mar"
+if %mm%==04 set Month="Apr"
+if %mm%==05 set Month="May"
+if %mm%==06 set Month="Jun"
+if %mm%==07 set Month="Jul"
+if %mm%==08 set Month="Aug"
+if %mm%==09 set Month="Sep"
+if %mm%==10 set Month="Oct"
+if %mm%==11 set Month="Nov"
+if %mm%==12 set Month="Dec"
 
- :: iterate over the folder structure in the "data" folder to get the databases
- for /d %%f in (*) do (
+set dirName=%dd%_%Month%_%yy%
+set fileSuffix=%dd%-%Month%-%yy%
 
- if not exist %backupDir%\%dirName%\ (
-      mkdir %backupDir%\%dirName%
- )
+:: remove echo here if you like
+echo "dirName"="%dirName%"
 
- %mysqldump% --host="localhost" --user=%dbUser% --password=%dbPassword% --single-transaction --add-drop-table --databases %%f > %backupDir%\%dirName%\%%f.sql
+:: switch to the "data" folder
+pushd "%mysqlDataDir%"
 
- %zip% a -tgzip %backupDir%\%dirName%\%fileSuffix%_%%f.sql.gz %backupDir%\%dirName%\%%f.sql
+:: create backup folder if it doesn't exist
+if not exist %backupDir%\%dirName%\   mkdir %backupDir%\%dirName%
 
- del %backupDir%\%dirName%\%%f.sql
- )
- popd
+:: iterate over the folder structure in the "data" folder to get the databases
+for /d %%f in (*) do (
+	:: remove echo here if you like
+	echo processing folder "%%f"
+
+	%mysqldump% --host="localhost" --user=%dbUser% --password=%dbPassword% --single-transaction --add-drop-table --databases %%f > %backupDir%\%dirName%\%%f.sql
+	%zip% a -tgzip %backupDir%\%dirName%\%fileSuffix%_%%f.sql.gz %backupDir%\%dirName%\%%f.sql
+	del %backupDir%\%dirName%\%%f.sql
+)
+popd
